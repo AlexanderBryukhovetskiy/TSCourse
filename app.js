948c628533ccbefa4a8f3094d5c56c11d288f525
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-07-29 22:44:44 +0300

    2-nd module

--- app.js
+++ app.js
@@ -7,3 +7,79 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+//practice #1
+var age;
+age = 50;
+var userName;
+userName = 'Max';
+var toggle;
+toggle = true;
+var empty;
+empty = null;
+var notInitialize;
+notInitialize = undefined;
+var callback;
+callback = function (a) { return 100 + a; };
+var anything = -20;
+anything = 'Text';
+anything = {};
+var some;
+some = 'Text';
+var str;
+if (typeof some === 'string') {
+    str = some;
+}
+;
+var person;
+person = ['Max', 21];
+var Status;
+(function (Status) {
+    Status[Status["LOADING"] = 0] = "LOADING";
+    Status[Status["READY"] = 1] = "READY";
+})(Status || (Status = {}));
+;
+var someAction = {
+    progress: Status.READY
+};
+if (someAction.progress === Status.READY) {
+    console.log('progress:', Status.READY);
+}
+var b;
+b = 35;
+b = 'hello';
+var isActive;
+function userStatus(isActive) {
+    if (isActive === 'enable') {
+        console.log('User is online');
+    }
+    else {
+        console.log('User is offline');
+    }
+    ;
+}
+;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error('Error');
+}
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: new Date('2021-01-01'),
+        updateAt: new Date('2021-05-01'),
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+};
